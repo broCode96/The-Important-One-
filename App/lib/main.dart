6d46ffd9d74ff27015c7e71d2570cdadc0a2f864
brocode96 <work.sankha@gmail.com>
2020-05-30 13:21:06 +0530

    Bro changes to new title

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Changed title changed by sankha'),
+      home: MyHomePage(title: 'New title'),
     );
   }
 }
